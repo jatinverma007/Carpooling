2207833a4886461a29570da70f25786383bd46e9
jatin_verma <jatin.verma@eroute.in>
2024-09-10 23:49:15 +0530

    fix

--- CPUserService/src/main/java/com/carpooling/ums/serviceImpl/UserServiceImpl.java
+++ CPUserService/src/main/java/com/carpooling/ums/serviceImpl/UserServiceImpl.java
@@ -72,46 +72,6 @@ public class UserServiceImpl implements UserService {
 		}
 	}
 
-//	@Override
-//	public ResponseEntity<?> createUser(UserDTO userDTO) {
-//	    try {
-//	        // Convert UserDetailsDTO to UserDetails
-//	        UserDetailsDTO userDetailsDTO = userDTO.getUserDetails();
-//	        UserDetails userDetails = convertToUserDetails(userDetailsDTO);
-//
-//	        // Convert AddressDTO list to Address entity list and set it in UserDetails
-//	        List<Address> addresses = convertToAddressList(userDTO.getAddresses(), userDetails);
-//	        userDetails.setAddresses(addresses);
-//
-//	        // Convert EmergencyContactDTO list to EmergencyContact entity list and set it in UserDetails
-//	        List<EmergencyContact> emergencyContacts = convertToEmergencyContactList(userDTO.getEmergencyContacts(), userDetails);
-//	        userDetails.setEmergencyContacts(emergencyContacts);
-//
-//	        // Save UserDetails first
-//	        UserDetails savedUserDetails = userDetailsRepository.save(userDetails);
-//
-//	        // Convert UserDTO to User and set UserDetails
-//	        User user = convertToUser(userDTO, savedUserDetails);
-//
-//	        // Save User entity
-//	        User savedUser = userRepository.save(user);
-//
-//	        // Convert saved user entity to UserDTO
-//	        UserDTO savedUserDTO = DtoConverter.convertToDto(savedUser, UserDTO.class);
-//
-//	        // Generate JWT token
-//	        String jwtToken = jwtUtil.generateToken(savedUser.getUsername());
-//
-//	        // Return UserCreationResponse with JWT token and UserDTO	        
-//            return ResponseEntity.ok(new AuthenticationResponse(jwtToken, savedUserDTO));
-//
-//
-//	    } catch (DataAccessException e) {
-//	        logger.error("Error occurred while creating user", e);
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse("404", "Unable to create user. Please try again later."));
-//	    }
-//	}
-
 	@Override
 	public ResponseEntity<?> createUser(UserDTO userDTO) {
 		try {
@@ -160,33 +120,6 @@ public class UserServiceImpl implements UserService {
                     .body("An unexpected error occurred. Please contact support if the issue persists.");
         }
 	}
-
-//	private UserDetails convertToUserDetails(UserDetailsDTO userDetailsDTO) {
-//		return DtoConverter.convertToEntity(userDetailsDTO, UserDetails.class);
-//	}
-//
-//	private List<Address> convertToAddressList(List<AddressDTO> addressDTOs, UserDetails userDetails) {
-//		List<Address> addresses = DtoConverter.convertToEntityList(addressDTOs, Address.class);
-//		addresses.forEach(address -> address.setUserDetails(userDetails));
-//		return addresses;
-//	}
-//
-//	private List<EmergencyContact> convertToEmergencyContactList(List<EmergencyContactDTO> emergencyContactDTOs,
-//			UserDetails userDetails) {
-//		return Optional.ofNullable(emergencyContactDTOs).filter(list -> !list.isEmpty())
-//				.map(list -> list.stream().map(dto -> {
-//					EmergencyContact contact = new EmergencyContact();
-//					contact.setName(dto.getName());
-//					contact.setPhoneNumber(dto.getPhoneNumber());
-//					contact.setRelationship(dto.getRelationship());
-//					contact.setEmail(dto.getEmail());
-//					contact.setAddress(dto.getAddress());
-//					contact.setPrimary(dto.isPrimary());
-//					contact.setCreatedAt(new Date());
-//					contact.setUserDetails(userDetails);
-//					return contact;
-//				}).toList()).orElse(Collections.emptyList());
-//	}
 
 	private User convertToUser(UserDTO userDTO) {
 		User user = new User();
